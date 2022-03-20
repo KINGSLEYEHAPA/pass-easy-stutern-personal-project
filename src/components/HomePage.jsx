9b9e1e27b4f0d9dd0a2aee68da17124a34b0173a
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-20 21:07:29 +0100

    done with writing Word of the done section

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -33,7 +33,7 @@ const HomePage = () => {
           initial="hidden"
           animate="visible"
           transition={{ duration: 1, type: "spring", stiffness: 120 }}
-          className="bg-green-200/80 h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2 lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
+          className="bg-green-200/80 hover:bg-white h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2 lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
         >
           <p className="h-14 p-1">
             <img className="h-full w-20 rounded-md" src={jambLogo} alt="Jamb" />{" "}
@@ -66,7 +66,7 @@ const HomePage = () => {
             stiffness: 120,
             delay: 0.4,
           }}
-          className="bg-green-200/80 h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2   lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
+          className="bg-green-200/80 hover:bg-white h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2   lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
         >
           <p className="h-14 p-1">
             <img className="h-full w-20 rounded-md" src={waecLogo} alt="Jamb" />{" "}
@@ -99,7 +99,7 @@ const HomePage = () => {
             stiffness: 120,
             delay: 0.8,
           }}
-          className="bg-green-200/80 h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2  lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
+          className="bg-green-200/80 hover:bg-white h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 lg:w-80 border-2  lg:border-r-2 border-gray-300/50 p-2 cursor-pointer"
         >
           <p className="h-14 p-1">
             <img className="h-full w-20 rounded-md" src={postUtme} alt="Jamb" />{" "}
@@ -132,7 +132,7 @@ const HomePage = () => {
             stiffness: 120,
             delay: 1.2,
           }}
-          className="bg-green-200/80  h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 border-2 border-gray-300/50  lg:w-80 p-2 cursor-pointer"
+          className="bg-green-200/80 hover:bg-white  h-56 w-full ssm:w-[20.8rem] md:w-96 lg:h-56 border-2 border-gray-300/50  lg:w-80 p-2 cursor-pointer"
         >
           <p className="h-14 p-1">
             <img
@@ -158,6 +158,68 @@ const HomePage = () => {
             </div>
           </Link>
         </motion.div>
+      </div>
+      <div className="h-[40rem] ssm:h-[30rem] w-full p-2 flex flex-col ">
+        <div className="h-14 bg-green-200/40 flex justify-center items-center">
+          <h2 className="text-2xl md:text-4xl text-green-500">
+            Word of the day
+          </h2>
+        </div>
+        <div className="flex h-full  ">
+          <div className="bg-green-500 w-1/2 ssm:w-1/3 h-full p-2 flex flex-col justify-start items-start">
+            <span className="text-2xl text-green-100">
+              {" "}
+              Annoy- <span className="text-2xl">/əˈnɔɪ/</span>
+            </span>
+
+            <div className="mt-4 text-white text-sm">
+              <p className="text-xl text-green-700">Synonyms</p>
+              <p>Bother</p>
+              <p>Bug</p>
+              <p className="text-xl text-green-700">Antonyms</p>
+              <p>Please</p>
+            </div>
+          </div>
+          <div className="bg-green-500 w-1/2 ssm:w-2/3 h-full p-2">
+            <div className="py-4">
+              <span className="bg-white text-green-500 px-2 py-1 rounded-sm font-bold ">
+                Noun
+              </span>
+              <p className="text-md ssm:text-xl text-white mt-4">
+                Definition:{" "}
+                <span className="text-sm ssm:text-lg text-white">
+                  A feeling of discomfort or vexation caused by what one
+                  dislikes.
+                </span>
+              </p>
+              <p className="text-green-700 text-md ssm:text-xl mt-2">
+                Example:{" "}
+                <span className="text-white text-sm ssm:text-lg">
+                  He is annoy
+                </span>
+              </p>
+            </div>
+            <div className="py-4">
+              <span className="bg-white text-green-500 px-2 py-1 rounded-sm font-bold ">
+                Verb
+              </span>
+              <p className="text-md ssm:text-xl text-white mt-4">
+                Definition:{" "}
+                <span className="text-sm ssm:text-lg text-white">
+                  To disturb or irritate, especially by continued or repeated
+                  acts; to bother with unpleasant deeds.
+                </span>
+              </p>
+              <p className="text-green-700 text-md ssm:text-xl mt-2">
+                Example:{" "}
+                <span className="text-white text-sm ssm:text-lg">
+                  Marc loved his sister, but when she annoyed him he wanted to
+                  switch her off.
+                </span>
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   );
