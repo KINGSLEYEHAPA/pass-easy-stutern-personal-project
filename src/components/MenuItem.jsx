02a26955775acb882aab3f1fdb17b6251c3427b0
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-22 19:26:11 +0100

    Working on performance section

---
+++ src/components/MenuItem.jsx
@@ -0,0 +1,18 @@
+import { motion } from "framer-motion";
+
+const MenuItem = ({ text, selected, onClick }) => (
+  <motion.div
+    className="cursor-pointer relative text-md md:text-lg text-white"
+    onClick={onClick}
+    animate={{ opacity: selected ? 1 : 0.5 }}
+  >
+    {text}
+    {selected && (
+      <motion.div
+        className="absolute top-full left-0 h-1 w-full rounded bg-white/80"
+        layoutId="underline"
+      />
+    )}
+  </motion.div>
+);
+export default MenuItem;
--- src/components/PerformanceStats.jsx
+++ src/components/PerformanceStats.jsx
@@ -1,19 +1,15 @@
-import React from "react";
+import { AnimateSharedLayout } from "framer-motion";
+import React, { useState } from "react";
 import perfLogo from "../assets/img/performance.jfif";
+import MenuItem from "./MenuItem";
 
 const PerformanceStats = () => {
-  const statsTab = [
-    "Last Quiz",
-    "Best Quiz Time",
-    "Worse Quiz Time",
-    "Best Score",
-    "Worse Score",
-    "Absolute Best",
-  ];
+  const [selected, setSelected] = useState(0);
+  const statsTab = ["Best Quiz Time", "Best Score", "Absolute Best"];
   return (
     <div className="mt-52 lg:mt-[8.5rem] bg-green-100 h-[30rem] w-full p-4">
       <div className="bg-green-300 h-full w-full">
-        <div className="h-28 md:h-20 md:w-full bg-green-700 flex justify-between">
+        <div className="h-24 md:h-20 md:w-full bg-green-700 flex justify-between">
           <div className="w-56 h-full flex gap-2 justify-start items-start md:items-center mt-6 md:mt-0 px-2">
             <div className="w-10 h-10 relative">
               <img
@@ -25,7 +21,22 @@ const PerformanceStats = () => {
             </div>
             <h1 className="text-green-100 text-xl ssm:text-2xl">Performance</h1>
           </div>
-          <div className="bg-orange-200 h-full w-96 md:w-[40rem]"></div>
+          <div className=" h-full w-96 md:w-[40rem] flex justify-end items-center pr-6">
+            <div className=" flex flex-col md:flex md:flex-row gap-0 md:gap-2 justify-end items-start">
+              <AnimateSharedLayout>
+                {statsTab.map((el, index) => {
+                  return (
+                    <MenuItem
+                      text={el}
+                      key={index}
+                      selected={selected === index}
+                      onClick={() => setSelected(index)}
+                    />
+                  );
+                })}
+              </AnimateSharedLayout>
+            </div>
+          </div>
         </div>
       </div>
     </div>
