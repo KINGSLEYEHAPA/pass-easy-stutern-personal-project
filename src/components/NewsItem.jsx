d68754b0530559df76a9352c23a134b077dc9a44
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-25 00:04:41 +0100

    Working on news api

--- src/components/NewsItem.jsx
+++ src/components/NewsItem.jsx
@@ -22,39 +22,43 @@ const NewsItem = () => {
           <h1 className="text-green-100  text-xl ssm:text-2xl">News</h1>
         </div>
       </div>
-      <div className="flex flex-col justify-start items-center p-4 pb-20">
-        <img
-          className="w-full w-full lg:full"
-          src={
-            oneNewsItem?.media
-              ? oneNewsItem?.media
-              : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3BsnxtyM161gr7oMXaII2SVx0qYLaO3siFw&usqp=CAU"
-          }
-          alt="Main News"
-        />
-        <div className="flex justify-start items-center mt-6">
-          <h1 className="text-xl ssm:text-2xl md:text-4xl mb-4 mt-2 text-green-500">
-            {oneNewsItem?.title}
-          </h1>{" "}
+      <div className="flex flex-col ssm:flex-row justify-center items-center p-4 pb-20 gap-10">
+        <div className=" w-full ssm:w-1/2 h-[25rem] rounded-lg ">
+          <img
+            className="w-full h-full rounded-lg "
+            src={
+              oneNewsItem?.media
+                ? oneNewsItem?.media
+                : "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQ3BsnxtyM161gr7oMXaII2SVx0qYLaO3siFw&usqp=CAU"
+            }
+            alt="Main News"
+          />
         </div>
+        <div className="w-full ssm:w-1/2 flex flex-col justify-start ">
+          <div className="flex justify-start items-center ">
+            <h1 className="text-xl ssm:text-2xl md:text-4xl mb-4 mt-2 text-green-500">
+              {oneNewsItem?.title}
+            </h1>{" "}
+          </div>
 
-        <div className="flex  justify-start items-center w-full gap-3 ">
-          {" "}
-          <p className="text-sm ssm:text-md md:text-lg lg:text-xl mb-4 mt-2 text-green-700 font-bold">
-            {oneNewsItem?.author}
-          </p>{" "}
-          <p className="text-sm ssm:text-md md:text-lg lg:text-xl mb-4 mt-2 text-green-700 font-bold">
+          <div className="flex  justify-start items-center w-full gap-3 ">
             {" "}
-            {oneNewsItem?.published_date}
+            <p className="text-sm ssm:text-md md:text-lg lg:text-xl mb-4 mt-2 text-green-700 font-bold">
+              {oneNewsItem?.author}
+            </p>{" "}
+            <p className="text-sm ssm:text-md md:text-lg lg:text-xl mb-4 mt-2 text-green-700 font-bold">
+              {" "}
+              {oneNewsItem?.published_date}
+            </p>
+          </div>
+          <p className="w-full text-green-700">
+            {oneNewsItem?.summary}{" "}
+            <span className="text-gray-600 font-bold">
+              {" "}
+              <a href={oneNewsItem?.link}>Read More...</a>
+            </span>
           </p>
         </div>
-        <p className="w-full text-green-700">
-          {oneNewsItem?.summary}{" "}
-          <span className="text-gray-600 font-bold">
-            {" "}
-            <a href={oneNewsItem?.link}>Read More...</a>
-          </span>
-        </p>
       </div>
     </div>
   );
