518c5b68a4c749d41e69c1cb1ccf13937c9039d3
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-24 20:24:23 +0100

    Working on news api

--- src/redux/reducers/dictionaryReducers.js
+++ src/redux/reducers/dictionaryReducers.js
@@ -15,7 +15,12 @@ const dictionaryReducer = (state = dictionaryData, action) => {
   const { type, payload } = action;
   switch (type) {
     case actionTypes.GET_RANDOM_WORD_MEANING_START:
-      return { ...state, isLoadingRandom: true };
+      return {
+        ...state,
+        isLoadingRandom: true,
+        isRandomWordError: false,
+        randomWordError: null,
+      };
     case actionTypes.GET_RANDOM_WORD_MEANING_SUCCESS:
       return { ...state, randomWord: payload, isLoadingRandom: false };
     case actionTypes.GET_RANDOM_WORD_MEANING_ERROR:
@@ -26,7 +31,12 @@ const dictionaryReducer = (state = dictionaryData, action) => {
         randomWordError: payload,
       };
     case actionTypes.GET_SEARCH_WORD_START:
-      return { ...state, isLoadingWord: true };
+      return {
+        ...state,
+        isLoadingWord: true,
+        isWordError: false,
+        wordError: null,
+      };
     case actionTypes.GET_SEARCH_WORD_SUCCESS:
       return { ...state, wordDefinition: payload, isLoadingWord: false };
     case actionTypes.GET_SEARCH_WORD_ERROR:
