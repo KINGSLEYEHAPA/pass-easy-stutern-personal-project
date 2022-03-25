6aca0a1ff3a7d0c3cc10c2d916dc308984ae123b
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-25 22:51:06 +0100

    writing question component

--- src/components/Question.jsx
+++ src/components/Question.jsx
@@ -11,12 +11,12 @@ const Question = () => {
         </div>
       </div>
       <div className="w-full h-1/6  p-4 bg-green-300">
-        <p className="text-sm sm:text-md lg:text-lg text-green-700">
+        <p className="text-sm sm:text-md text-green-700">
           <strong>Q:</strong> What is the IUPAC Name of H2SO4?
         </p>
       </div>
       <div className="w-full h-3/6 bg-green-200">
-        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-300 h-1/5 cursor-pointer">
+        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-200 h-1/5 cursor-pointer">
           {" "}
           <div className="px-3 py-1  ">
             {" "}
@@ -24,7 +24,7 @@ const Question = () => {
           </div>
           <p>This is the choice 1</p>{" "}
         </div>
-        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-300 h-1/5 cursor-pointer">
+        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-200 h-1/5 cursor-pointer">
           {" "}
           <div className="px-3 py-1  ">
             {" "}
@@ -32,7 +32,7 @@ const Question = () => {
           </div>
           <p>This is the choice 1</p>{" "}
         </div>
-        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-300 h-1/5 cursor-pointer">
+        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-200 h-1/5 cursor-pointer">
           {" "}
           <div className="px-3 py-1  ">
             {" "}
@@ -40,7 +40,7 @@ const Question = () => {
           </div>
           <p>This is the choice 1</p>{" "}
         </div>
-        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-300 h-1/5 cursor-pointer">
+        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-200 h-1/5 cursor-pointer">
           {" "}
           <div className="px-3 py-1 ">
             {" "}
@@ -48,7 +48,7 @@ const Question = () => {
           </div>
           <p>This is the choice 1</p>{" "}
         </div>
-        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-300 h-1/5 cursor-pointer">
+        <div className="w-full p-4 flex justify-start items-center gap-2 hover:bg-gray-200 h-1/5 cursor-pointer">
           {" "}
           <div className="px-3 py-1  ">
             {" "}
@@ -57,7 +57,11 @@ const Question = () => {
           <p>This is the choice 1</p>{" "}
         </div>
       </div>
-      <div className="w-full h-1/6 bg-green-300"></div>
+      <div className="w-full h-1/6 bg-green-300 flex justify-center items-center">
+        <button className="bg-green-700 px-6 ssm:px-8 py-1 text-green-100 font-bold rounded md:rounded-md text-md ssm:text-xl outline-none">
+          Next
+        </button>
+      </div>
     </div>
   );
 };
