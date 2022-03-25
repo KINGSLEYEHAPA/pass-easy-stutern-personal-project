36fa91bb52b8f822116aa6d0a7b1aaa87d4e15f3
KINGSLEYEHAPA <kessity09@gmail.com>
2022-03-25 23:28:19 +0100

    writing the end components

--- src/components/End.jsx
+++ src/components/End.jsx
@@ -4,15 +4,25 @@ const End = () => {
   return (
     <div className="w-full h-full bg-green-300">
       <div className="w-full h-5/6 flex justify-center items-center">
-        <div className="w-4/5 h-4/6 bg-green-100 p-4 md:pl-10 flex justify-center items-start ">
-          <p className="mt-12 text-sm ssm:text-lg lg:text-xl">
+        <div className="w-5/6 h-4/6 bg-green-200 rounded p-4 md:pl-10 flex justify-center items-start ">
+          <p className="mt-12 text-sm ssm:text-lg lg:text-xl text-green-500">
             You scored <strong>88%</strong> in the{" "}
             <strong>Chemistry-UTME-2019</strong> quiz,You finished the quiz in{" "}
             <strong>12mins and 55secs.</strong>
           </p>{" "}
         </div>
       </div>
-      <div className="w-full h-1/6 bg-fuchsia-500"></div>
+      <div className="w-full h-1/6 flex justify-center items-center gap-5">
+        <button className="bg-green-700 px-4 ssm:px-8 py-1 text-green-100 rounded md:rounded-md text-sm  text-md ssm:text-lg outline-none">
+          Quiz
+        </button>
+        <button className="bg-green-700 px-4 ssm:px-8 py-1 text-green-100 rounded md:rounded-md text-sm  text-md ssm:text-lg outline-none">
+          Retry
+        </button>
+        <button className="bg-green-700 px-4 ssm:px-8 py-1 text-green-100  rounded md:rounded-md text-sm  text-md ssm:text-lg outline-none">
+          See Answers
+        </button>
+      </div>
     </div>
   );
 };
